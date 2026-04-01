5cfe8f96dbac99be32464fe81fdbcba1c4531c12
Jenny S-T <41115621+jennyst12@users.noreply.github.com>
2026-04-01 19:22:13 +0100

    Colours

--- game.js
+++ game.js
@@ -35,8 +35,8 @@
     { id: 21, base: "#1B5E20", pattern: "diag-stripes", patCol: "#0a3a10", name: "darkgreen" }, // very dark green, distinct from forest/lime
     { id: 22, base: "#37474F", pattern: "h-stripes",    patCol: "#1c2b31", name: "slate"    }, // blue-grey slate, unique hue in palette
     { id: 23, base: "#ff33ff", pattern: "h-stripes",    patCol: "#7C4DFF", name: "lilac"    },
-    { id: 24, base: "#6666ff", pattern: "h-stripes",    patCol: "#1c2b31", name: "sky"    },
-    { id: 25, base: "#ff33aa", pattern: "h-stripes",    patCol: "#1c2b31", name: "babypink"    },
+    { id: 24, base: "#AEC6CF", pattern: "h-stripes",    patCol: "#1c2b31", name: "sky"    },
+    { id: 25, base: "#ffb7c5", pattern: "h-stripes",    patCol: "#1c2b31", name: "babypink"    },
   ];
 
   function patternCSS(type, col) {
